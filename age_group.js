0cfdbcf56c7219cb30b6d1d7e033e940aaf98fc2
Anton Cherkasov <anton.cherkasov@gmail.com>
2018-11-29 21:44:10 +0300

    Age group

---
+++ age_group.js
@@ -0,0 +1,17 @@
+var age = 5;
+var ageGroup;
+
+if (age <= 1) {
+  console.log(ageGroup = 'Kittens'); 
+}
+else {
+ if (age > 1 && age <= 3) {
+   console.log(ageGroup = 'Young cats');
+ }
+ if (age > 3 && age <= 7) {
+   console.log(ageGroup = 'Cats of middle age');
+ }
+ if (age > 7) {
+   console.log(ageGroup = 'Venerable cats'); 
+ }
+}
