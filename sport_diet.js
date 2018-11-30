bb663ac75baa35364ccfc1ae43ac7427a693b175
Anton Cherkasov <anton.cherkasov@gmail.com>
2018-11-30 21:32:04 +0300

    Sport diet

---
+++ sport_diet.js
@@ -0,0 +1,15 @@
+var weight = 5;
+var length = 60;
+var age = 5;
+var activityRate = 1.725;
+var calorieRate;
+
+var metabolism = 88.362 + (13.397 * weight) + (4.799 * length) - (5.677 * age);
+
+console.log(metabolism);
+
+calorieRate = metabolism * activityRate;
+
+var proteins = Math.round(calorieRate * 0.40);
+var fats = Math.round(calorieRate * 0.25);
+var carbohydrates = Math.round(calorieRate * 0.35);
