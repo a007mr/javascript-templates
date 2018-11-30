3b5823e1eb86fa149b7d0e6f5f2e4b996b7dbb2c
Anton Cherkasov <anton.cherkasov@gmail.com>
2018-11-30 21:32:11 +0300

    Perimeter

---
+++ perimeter.js
@@ -0,0 +1,9 @@
+var firstLeg = 300;
+var secondLeg = 700;
+var perimeter;
+var hypotenuse;
+
+
+hypotenuse = Math.sqrt(firstLeg * firstLeg + secondLeg * secondLeg);
+perimeter = firstLeg + secondLeg + hypotenuse;
+perimeter = Math.round(perimeter);
