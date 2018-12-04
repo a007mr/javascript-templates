53d4db6cb84ba6c9f0da3a021506ae37a31a7157
Anton Cherkasov <anton.cherkasov@gmail.com>
2018-12-04 22:59:27 +0300

    For Loop finds dividers

---
+++ dividers.js
@@ -0,0 +1,9 @@
+var number = 30;
+
+for (var i = 2; i < number; i++) {
+  if (number % i === 0) {
+  console.log(i)}
+}
+
+
+// A program that finds all the divisors of a number, except one and the number itself.
