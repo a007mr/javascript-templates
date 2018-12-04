c4c4334c74937c8c5f09fdf0237d58d2cc96e237
Anton Cherkasov <anton.cherkasov@gmail.com>
2018-12-04 23:01:08 +0300

    while Loop determines digits in number

---
+++ amount_of_numbers.js
@@ -0,0 +1,11 @@
+var number = 123;
+var quantity = 1;
+
+while (number / 10 > 1) {
+quantity ++;
+number /= 10;
+}
+
+console.log(quantity);
+
+// A program that determines how many digits in one number
